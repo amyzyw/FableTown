403fc84cbb2b6b7678f9520a19635b2dee061695
Nuo-Cen <cenn.emily@gmail.com>
2024-04-15 21:31:47 -0400

    insert form added

--- frontend/src/pages/Insert.tsx
+++ frontend/src/pages/Insert.tsx
@@ -1,7 +1,54 @@
-const InsertPage = () => (
+import * as React from 'react';
 
-    <h1>Add a Place</h1>
-    
-);
+interface Props {
+    initialValue?: string;
+  }
+  
+  interface State {
+    value: string;
+  }
+  
 
-export default InsertPage;
+class NameForm extends React.Component<Props, State> {
+    constructor(props: Props) {
+      super(props);
+      this.state = { value: '' };
+  
+      this.handleChange = this.handleChange.bind(this);
+      this.handleSubmit = this.handleSubmit.bind(this);
+    }
+  
+    handleChange(event: React.ChangeEvent<HTMLInputElement>) {
+      this.setState({value: event.target.value});
+    }
+  
+    handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+      alert('A name was submitted: ' + this.state.value);
+      event.preventDefault();
+    }
+  
+    render() {
+      return (
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Name of the place:
+            <input type="text" value={this.state.value} onChange={this.handleChange} />
+          </label>
+          <label>
+            Location of the place:
+            X:
+            <input type="number" value={this.state.value} onChange={this.handleChange} />
+            Y:
+            <input type="number" value={this.state.value} onChange={this.handleChange} />
+          </label>
+          <label>
+            Description:
+            <input type="text" value={this.state.value} onChange={this.handleChange} />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
+      );
+    }
+  }
+
+  export default NameForm;
