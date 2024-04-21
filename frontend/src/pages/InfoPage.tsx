eff2ae7acc684d1690ff891901011d17b98ad713
Tenny-George124 <sog5@cornell.edu>
2024-04-21 03:40:26 -0400

    Rough "Edit" and "Delete" InfoPage Changes

--- frontend/src/pages/InfoPage.tsx
+++ frontend/src/pages/InfoPage.tsx
@@ -3,7 +3,7 @@ import { useEffect, useState } from "react";
 import { BACKEND_BASE_PATH } from "../constants/Navigation";
 
 
-type Props<T> = {
+type DuchyInfoProps<T> = {
     name?: string;
     description?: string
     x?: number
@@ -15,25 +15,54 @@ type Props<T> = {
 //     fetch(`${BACKEND_BASE_PATH}/duchy-info`).then((res) => res.json());
 
 // display duchy info page
-const DuchyInfo= <T extends { name: string, description: string}>({name, description, x, y}: Props<T>) => {
+const DuchyInfo = <T extends { name: string, description: string }>({
+    name,
+    description,
+    x,
+    y,
+}: DuchyInfoProps<T>) => {
+
+    const [editName, setEditName] = useState(name || '');
+    const [editDescription, setEditDescription] = useState(description || '');
+
     // useEffect to load the duchy info 
     useEffect(() => {
+       // fetch the dutchy info from the backend
+       //fetch(`${BACKEND_BASE_PATH}/duchy-info`).then((res) => res.json());
+    }, []);
 
     // Where the edit and delete codes should go
-        // const editInfo () => {}
-        // UPDATE
+    const editInfo () => {
+        console.log("Editing Your Duchy's Info:", editName, editDescription);
+    };
 
-        // const deleteInfo() => {}
-        // DELETE
-    
-    }, []);
+    const deleteInfo() => {
+        console.log("Deleting Your Duchy:", name);
+    };
+
+    const controlInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        //update the state with setSearch (function provided by React's useState)
+        setEditName(event.target.value);
+        setEditDescription(event.target.value);  
+        // whenever the user types in the search input field, search state gets 
+        // updated to reflect current value of the input.
+        // setPage(1); // Resets back to page 1 when search is changed
+    };
 
     return (
         <div>
             <h1>{name}</h1>
             <p>{description}</p>
-            <button>Edit</button>
-            <button>Delete</button>
+            <input type='text' value={editName} 
+                onChange={controlInputChange}
+                duchyNewName='New Name'
+            />
+            <textarea value={editDescription} 
+                onChange={controlInputChange}
+                duchyNewDescrip='New Description'
+            />
+            <button onClick={editInfo}>Edit</button>
+            <button onClick={deleteInfo}>Delete</button>
         </div>
     );
 };
